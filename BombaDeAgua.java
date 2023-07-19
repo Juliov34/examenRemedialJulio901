bbea1ca88960a1e92144105444588601b9f03160
Juliov34 <juliovr34@gmail.com>
2023-07-19 16:03:13 -0600

    Cuarto commit donde se agregan los atributos de la bomba de agua y el método constructor.

--- BombaDeAgua.java
+++ BombaDeAgua.java
@@ -1,3 +1,12 @@
 public class BombaDeAgua{
     //Aquí se encuentra la bomba de agua.
+    private int capacidadNecesaria;
+    private int voltaje;
+    private int espacioDisponible;
+
+    public BombaDeAgua(int capacidadNecesaria, int voltaje, int espacioDisponible) {
+        this.capacidadNecesaria = capacidadNecesaria;
+        this.voltaje = voltaje;
+        this.espacioDisponible = espacioDisponible;
+    }
 }
