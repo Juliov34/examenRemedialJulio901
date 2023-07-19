6e5ac85f42f0da89417450f796161515ed655cb6
Juliov34 <juliovr34@gmail.com>
2023-07-19 16:01:43 -0600

    Tercer commit donde se agregan los atributos de la batería y el método constructor del mismo.

--- Bateria.java
+++ Bateria.java
@@ -1,3 +1,12 @@
 public class Bateria {
-    //Aquí se encuentra la batería
+    //Aquí se encuentra la batería}
+    public int capacidadNecesaria;
+    public int voltaje;
+    public int espacioDisponible;
+
+    public EjemploConstructor(int capacidadNecesaria, int voltaje, int espacioDisponible) {
+        this.capacidadNecesaria = capacidadNecesaria;
+        this.voltaje = voltaje;
+        this.espacioDisponible = espacioDisponible;
+    }
 }
